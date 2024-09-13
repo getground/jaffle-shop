11e5cfd7b1550ae07081fcb77541ae87b79bbc53
Mariana Fernandes <mariana.fernandes@getground.co.uk>
2024-09-13 17:46:24 +0100

    comment field

--- models/marts/customers.sql
+++ models/marts/customers.sql
@@ -18,7 +18,7 @@ customer_orders_summary as (
         orders.customer_id,
 
         count(distinct orders.order_id) as count_lifetime_orders,
-        --count(distinct orders.order_id) > 1 as is_repeat_buyer,
+        count(distinct orders.order_id) > 1 as is_repeat_buyer,
         min(orders.ordered_at) as first_ordered_at,
         max(orders.ordered_at) as last_ordered_at,
         sum(orders.subtotal) as lifetime_spend_pretax,
@@ -41,12 +41,12 @@ joined as (
         customer_orders_summary.last_ordered_at,
         customer_orders_summary.lifetime_spend_pretax,
         customer_orders_summary.lifetime_tax_paid,
-        customer_orders_summary.lifetime_spend,
+        customer_orders_summary.lifetime_spend
 
-        case
+        /*case
             when customer_orders_summary.is_repeat_buyer then 'returning'
             else 'new'
-        end as customer_type
+        end as customer_type*/
 
     from customers
 
